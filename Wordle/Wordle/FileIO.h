141a34712a9689ddeba4b7d647cf9eab8ab942d8
Vlad Semenov <71037935+W4nttt3d@users.noreply.github.com>
2025-02-07 18:26:25 +0200

    Revert "Complete Workshop 2"

--- Wordle/Wordle/FileIO.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <string>
-void readListWords(const char* fileName, std::string* array, unsigned size);
-
-void readFile(const char* fileName, int& gamesPlayed, int& gamesWon, int& gamesLost, int& points, std::string& dateTime);
-
-void writeFile(const char* fileName, int& gamesPlayed, int& gamesWon, int& gamesLost, int& points, std::string& dateTime);
--- Wordle/Wordle/GameManager.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>;
-
-void wordleOfTheDay(int& gamesPlayed, int& gamesWon, int& gamesLost, int& points, std::string& dateTime);
-
-void randomWordle(int& gamesPlayed, int& gamesWon, int& gamesLost, int& points);
-
-void gameStatistics(int& gamesPlayed, int& gamesWon, int& gamesLost, int& points);
